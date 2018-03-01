6f89e578012a5c09165dbbb7d3d387018f721337
adarsh301998 <devil301998@gmail.com>
2018-03-02 02:06:17 +0530

    Revert "change 2"
    This reverts commit 9c6cce8

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,9 +12,6 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-
-        System.out.println("Hello World");
-
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
